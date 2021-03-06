b84444af91eb3baaa82a0c580b1827ae029b3b46
Cristiano Cunha <contato@cristianoprogramador.com>
2021-03-06 11:00:14 -0300

    Exemplo de classe e construtor

--- 12.Classes.py
+++ 12.Classes.py
@@ -2,11 +2,14 @@ class Pessoa:
     def __init__(self):
         print("Construtor padrão")
 
-    def __init__(self, nome = "Cristiano"):
+    def __init__(self, nome="Cristiano"):
         print(f"Construtor modificado - {nome}")
+        self.nome = nome
 
     def falar(self):
         print("Olá Mundo")
 
+
 p1 = Pessoa()
 p1.falar()
+print(p1.nome)
